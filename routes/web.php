cfe0a5771356ddc0dc2f71613b285ad2757679c1
hamza01mir <67631738+hamza01mir@users.noreply.github.com>
2020-07-29 15:14:40 +0500

    commented out a few routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// });
 
 
 
