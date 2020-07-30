381600a0983529a4acddbc83e36ea50d62120b06
hamza01mir <67631738+hamza01mir@users.noreply.github.com>
2020-07-30 16:30:07 +0500

    employees.index showing table of employees

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,1 +1,34 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('content')
+<div>
+    <div class="container">
+        <table id="employees-table" class="table table-striped">
+			<thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>CNIC</th>
+                <th>Phone</th>
+                <th>Address</th>
+            </tr>
+			</thead>
+			<tbody>
+            @foreach($employees as $employee)
+            <tr>
+                <td>{{ $employee->name }}</td>
+                <td>{{ $employee->user->email }}</td>
+                <td>{{ $employee->cnic }}</td>
+                <td>{{ $employee->phone }}</td>
+                <td>{{ $employee->address }}</td>
+            </tr>
+            @endforeach
+			</tbody>
+        </table>
+    </div>
+</div>
+
+<script>
+	$(function () {
+		$("#employees-table").DataTable();
+	});
+</script>
+@endsection
