6fc6623b628355bf804140f27f5a84094ee7715b
hamza01mir <67631738+hamza01mir@users.noreply.github.com>
2020-08-01 05:35:37 +0500

    align front layout for create.blade.php

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -2,29 +2,42 @@
 
 
 @section('content')
-
-<form method="POST" action="{{ route('employees.store') }}">
+	<div class="container">
+		<form method="POST" action="{{ route('employees.store') }}">
 	@csrf
-	<div class="form-group">
-		<label for="name">Name</label>
-		<input class="form-control" name="name" id="name"/>
+	<div class="row">
+		<div class="form-group col-md-6">
+			<label for="name">Name:</label>
+			<input class="form-control" name="name" id="name"/>
+		</div>
+		<div class="form-group col-md-6">
+			<label for="email">Email:</label>
+			<input type="email" class="form-control" name="email" id="email"/>
+		</div>
+		<div class="form-group col-md-6">
+			<label for="password">Password:</label>
+			<input class="form-control" name="password" id="name"/>
+		</div>
+		<div class="form-group col-md-6">
+			<label for="confirm-password">Confrirm Password::</label>
+			<input type="email" class="form-control" name="confirm-password" id="email"/>
+		</div>
+
+		<div class="form-group col-lg-12">
+			<label for="address">Address:</label>
+			<input class="form-control" name="address" id="phone"/>
+		</div>
+		<div class="form-group col-md-4">
+			<label for="phone">Phone:</label>
+			<input class="form-control" name="phone" id="address"/>
+		</div>
+		<div class="form-group col-md-4">
+			<label for="cnic">CNIC:</label>
+			<input class="form-control" name="cnic" id="cnic"/>
+		</div>
 	</div>
-	<div class="form-group">
-		<label for="email">Email</label>
-		<input type="email" class="form-control" name="email" id="email"/>
-	</div>
-	<div class="form-group">
-		<label for="phone">Phone</label>
-		<input class="form-control" name="phone" id="phone"/>
-	</div>
-	<div class="form-group">
-		<label for="address">Address</label>
-		<input class="form-control" name="address" id="address"/>
-	</div>
-	<div class="form-group">
-		<label for="cnic">CNIC</label>
-		<input class="form-control" name="cnic" id="cnic"/>
-	</div>
-	<input class="btn btn-primary" type="submit" name="submit" id="submit" />
-</form>
+		<input class="btn btn-primary" type="submit" name="submit" id="submit" />
+
+					
+	
 @endsection
