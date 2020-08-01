0b17680471d043f5215f828833bcd083d1fac904
Hassan <hassanhafiz44@gmail.com>
2020-08-01 05:43:30 +0500

    created productImages table migration

---
+++ database/migrations/2020_08_01_053442_create_product_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+						$table->bigInteger('product_id', false, true);
+						$table->string('image_uri');
+						$table->foreign('product_id')
+							->references('id')
+							->on('products')
+							->onDelete('cascade')
+							->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_images');
+    }
+}
