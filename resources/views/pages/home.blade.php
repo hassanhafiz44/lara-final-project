0fbd3bf75159ef8cb97d28ced778a92406decbed
hamza01mir <67631738+hamza01mir@users.noreply.github.com>
2020-08-03 00:26:38 +0500

    Add image section to show.blade.ph

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,27 +1,38 @@
 @extends('layouts.app')
 
 @section('stylesheets')
-<link rel="stylsheet" href="{{ asset('css/home.css') }}">
+ <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+<div class="page-content">
+      Page Content
     </div>
-</div>
+<footer>
+      <div class="footer-container">
+        <div class="left-col">
+          
+          <div class="social-media">
+            <a href="#"><i class="fa fa-facebook-f"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+            <a href="#"><i class="fa fa-youtube"></i></a>
+            <a href="#"><i class="fa fa-linkedin-in"></i></a>
+          </div>
+          <p class="rights-text">© 2020 All Rights Reserved.</p>
+        </div>
+
+        <div class="right-col">
+          <h1>Our Newsletter</h1>
+          <div class="border"></div>
+          <p>Enter Your Email to get our news and updates.</p>
+          <form action="" class="newsletter-form">
+            <input type="text" class="txtb" placeholder="Enter Your Email">
+            <input type="submit" class="btn" value="submit">
+          </form>
+        </div>
+      </div>
+    </footer>
+
 @endsection
