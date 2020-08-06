0e17798909e8209ec3e4db46d144352aacc0d2ba
Hassan <hassanhafiz44@gmail.com>
2020-08-06 18:33:13 +0500

    added Product relations

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    public function images()
+    {
+    	return $this->hasMany('App\ProductImage');
+    }
+
+	 public function category()
+	 {
+		 return $this->belongsTo('App\ProductCategory');
+	 }
 }
