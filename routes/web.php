afd4c320c43164d3b6473da522f88cf1bf976103
Hassan <hassanhafiz44@gmail.com>
2020-09-12 15:44:32 +0500

    fixed web routes

--- routes/web.php
+++ routes/web.php
@@ -27,27 +27,39 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/customers')->name('customers.')->namespace('Customers\Auth')->group(function () {
     Route::get('/login', 'LoginController@showLoginForm')->name('login');
     Route::post('/login', 'LoginController@login')->name('login.attempt');
+    Route::get('/logout', 'LoginController@logout')->name('logout');
+    Route::get('/register', 'RegisterController@showRegistrationForm')->name('register');
+    Route::post('/register', 'RegisterController@register');
+});
+
+Route::prefix('/users')->name('users.')->namespace('Auth')->group(function () {
+    Route::get('/login', 'LoginController@showLoginForm')->name('login');
+    Route::post('/login', 'LoginController@login')->name('login.attempt');
     Route::post('/logout', 'LoginController@logout')->name('logout');
     Route::get('/register', 'RegisterController@showRegistrationForm')->name('register');
     Route::post('/register', 'RegisterController@register');
 });
 
+Route::prefix('/pages')->name('pages.')->group(function () {
+    Route::get('/products', 'PagesController@products')->name('products');
+    Route::get('/services', 'PagesController@services')->name('services');
+    Route::get('/about', 'PagesController@about')->name('about');
+    Route::get('/contact', 'PagesController@contact')->name('contact');
+});
 
-<<<<<<< HEAD
-
-Route::get('/','PagesController@index')->name('pages.index');
-Route::get('pages/contact','PagesController@contact')->name('pages.contact');
-Route::get('pages/products','PagesController@products')->name('pages.products');
-Route::get('pages/services','PagesController@services')->name('pages.services');
-Route::get('pages/about','PagesController@about')->name('pages.about');
-=======
-Route::get('/', 'HomeController')->name('home');
 Route::get('/', 'PagesController@index')->name('pages.index');
-Route::get('contact', 'PagesController@contact')->name('pages.contact');
-Route::get('products', 'PagesController@products')->name('pages.products');
-Route::get('services', 'PagesController@services')->name('pages.services');
-Route::get('about', 'PagesController@about')->name('pages.about');
->>>>>>> master
+// Route::get('pages/contact', 'PagesController@contact')->name('pages.contact');
+// Route::get('pages/products', 'PagesController@products')->name('pages.products');
+// Route::get('pages/services', 'PagesController@services')->name('pages.services');
+// Route::get('pages/about', 'PagesController@about')->name('pages.about');
+
+// Route::get('/', 'HomeController')->name('home');
+// Route::get('/', 'PagesController@index')->name('pages.index');
+// Route::get('contact', 'PagesController@contact')->name('pages.contact');
+// Route::get('products', 'PagesController@products')->name('pages.products');
+// Route::get('services', 'PagesController@services')->name('pages.services');
+// Route::get('about', 'PagesController@about')->name('pages.about');
+
 // Route::get('profile','Employees@profile');
 // Route::post('loginsubmit','Employees@loginaction');
 // Route::get('login','Employees@adminlogin');
@@ -57,7 +69,7 @@ Route::get('about', 'PagesController@about')->name('pages.about');
 
 
 
-Auth::routes();
+//Auth::routes();
 
 Route::resource('employees', 'EmployeesController');
 Route::resource('product_categories', 'ProductCategoriesController');
