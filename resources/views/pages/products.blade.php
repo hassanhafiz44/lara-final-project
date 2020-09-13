4c28158225562d88e6d88a410397de42f731a8f6
Hassan <hassanhafiz44@gmail.com>
2020-09-13 20:25:09 +0500

    adding products to cart and changing their quantity

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -7,6 +7,7 @@
 @section('content')
 <div class="container">
 	<h1 class="text-center">Our Products</h1>
+	<a href="#" data-toggle="modal" data-target="#cart-modal">Cart</a>
 
 	<div class="row">
 		@foreach($products as $product)
@@ -157,6 +158,29 @@
 
 </footer>
 <script>
+	function addProductToCart(product) {
+		const productInput = `
+			<div class="row" id="product-input-${product.id}">
+				<div class="form-group col-md-6">
+					<label for="product-${product.id}-${product.name}">Name</label>
+					<input type="hidden" type="number" name="product_ids[]" id="product-${product.id}">
+					<input id="product-${product.id}-${product.name}" disabled type="text" name="product_names[]" class="form-control" value="${product.name}">
+				</div>
+				<div class="form-group col-md-6">
+					<label for="product-${product.id}-quantity">Quantity</label>
+					<input type="number" name="product_quantities[]" id="product-${product.id}-quantity" class="form-control" value="1">
+				</div>
+			</div>
+		`;
+
+		$("#cart-form").append(productInput);
+	}
+
+	function removeProductFromCart(product) {
+		$(`#product-input-${product.id}`).remove();
+	}
+</script>
+<script>
 	$(function() {
 		$(".product-link").on('click', function(event) {
 			const product = $(event.target).closest('.product');
@@ -176,6 +200,8 @@
 				buyBtn.children("span").text("ADDED");
 				buyBtn.data("action", "bought");
 				removeBtn.removeClass("d-none");
+
+				addProductToCart(product.data());
 			} else {
 				console.log("Product already added.");
 			}
@@ -196,6 +222,8 @@
 				buyBtn.children("span").text("BUY");
 				buyBtn.data("action", "buy");
 				removeBtn.addClass("d-none");
+
+				removeProductFromCart(product.data());
 			} else {
 				console.log("Product already removed.");
 			}
