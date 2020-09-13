9e95bd30d3449b26aa861a6fb0e89e0123322793
Hassan <hassanhafiz44@gmail.com>
2020-09-13 20:26:22 +0500

    added models related to invoice

---
+++ app/InvoiceProduct.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceProduct extends Model
+{
+    //
+    public function product()
+    {
+        return $this->hasOne('App\Product');
+    }
+}
