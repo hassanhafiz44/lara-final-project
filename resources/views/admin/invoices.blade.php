a246599a3d3812890a4ef764d7fc283b7af06b99
Hassan <hassanhafiz44@gmail.com>
2020-09-24 19:43:49 +0500

    added adminInvoicesController and invoices index

---
+++ resources/views/admin/invoices.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+    <div class="container">
+        <table id="invoices-table" class="table table-striped">
+            <thead>
+                <tr>
+                    <!-- <th>Product</th> -->
+                     <th>Customer</th>
+                    <th>Retail Price</th>
+                    <th>Payment Status</th>
+                    <th>Invoice Status</th>
+                    <!-- <th>Quantity</th> -->
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($invoices as $invoice)
+                <tr data-id="{{ $invoice->id }}">
+                     <td>{{ $invoice->customer->name }}</td>
+                    <td>{{ $invoice->retail_price_total }}</td>
+                    <td>{{ $invoice->payment_status }}</td>
+                    <td>{{ $invoice->invoice_status }}</td>
+                    <!-- <td>{{ $invoice->quantity }}</td> -->
+                    <td>
+                        <a class="btn btn-sm btn-secondary" href="{{ route('pages.invoices.edit', $invoice->id) }}"><i class="fa fa-edit"></i></a>
+                        <a class="btn btn-sm btn-warning" href="{{ route('pages.invoices.show', $invoice->id) }}"><i class="fa fa-eye"></i></a>
+                        <!-- <button class="btn btn-sm btn-danger delete-product"><i class="fa fa-trash"></i></button> -->
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
