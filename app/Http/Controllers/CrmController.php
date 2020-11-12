a967b103b9c0b706d4bd682bd0a9e016829565b4
Hassan <hassanhafiz44@gmail.com>
2020-11-12 16:53:14 +0500

    Added filters to CRM

--- app/Http/Controllers/CrmController.php
+++ app/Http/Controllers/CrmController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
+use App\Invoice;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,12 +15,28 @@ class CrmController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
         $data = array('title' => 'CRM');
-        $data['customers'] = DB::table('customers')->select(['id', 'name', 'email', 'cnic', 'address', 'phone', 'status'])->get();
-        // var_dump($data); die();
+        $customers = DB::table('customers as c')
+            ->leftJoin('invoices as i', 'c.id' , '=', 'i.customer_id')
+            ->select(['c.id', 'c.name', 'c.email', 'c.cnic', 'c.address', 'c.phone', 'c.status', 'i.payment_status']);
+
+        $data['is_active'] = "";
+        $data['payment_status'] = "";
+        
+        if($request->filled('is_active')) {
+            $customers = $customers->where('c.status', '=', $request->is_active);
+            $data['is_active'] = $request->is_active;
+        }
+        if($request->filled('payment_status')) {
+            $customers = $customers->where('i.payment_status', '=', $request->payment_status);
+            $data['payment_status'] = $request->payment_status;
+        }
+        $customers = $customers->groupByRaw('c.id')->get();
+
+        $data['customers'] = $customers;
         return view('crm.index')->with($data);
     }
 
@@ -51,6 +70,24 @@ class CrmController extends Controller
     public function show($id)
     {
         //
+        try{
+            $customer = Customer::findOrFail($id);
+            $paid_invoices = $customer->paid_invoices;
+            $unpaid_invoices = $customer->unpaid_invoices;
+            $amount_due = $unpaid_invoices->sum('retail_price_total');
+            $amount_paid = $paid_invoices->sum('retail_price_total');
+
+            $data['customer'] = $customer;
+            $data['paid_invoices'] = $paid_invoices;
+            $data['unpaid_invoices'] = $unpaid_invoices;
+            $data['amount_due'] = $amount_due;
+            $data['amount_paid'] = $amount_paid;
+
+
+            return view('crm.viewcustomer', $data);
+        } catch (ModelNotFoundException $e) {
+            abort(404);
+        }
     }
 
     /**
@@ -85,5 +122,41 @@ class CrmController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function set_inactive(Request $request)
+    {
+       try {
+          $customer = Customer::findOrFail($request->id);
+          $customer->status = 'inactive';
+          $customer->save();
+          return response()->json(['message' => 'Status changed to inactive',
+             'status' => $customer->status
+          ], 200);
+       } catch (ModelNotFoundException $e) {
+          return response()->json([
+             'error' => [
+                'message' => 'Customer not found'
+             ]
+          ],404);
+       }
+    }
+
+    public function set_active(Request $request)
+    {
+       try {
+          $customer = Customer::findOrFail($request->id);
+          $customer->status = 'active';
+          $customer->save();
+          return response()->json(['message' => 'Status changed to inactive',
+             'status' => $customer->status
+          ], 200);
+       } catch (ModelNotFoundException $e) {
+          return response()->json([
+             'error' => [
+                'message' => 'Customer not found'
+             ]
+          ],404);
+       }
     }
 }
--- resources/views/crm/index.blade.php
+++ resources/views/crm/index.blade.php
@@ -1,9 +1,33 @@
 @extends('layouts.app') @section('content')
-<div>
+<div ng-app="crm" ng-controller="CRMCtrl">
     <div class="container">
+        <form action="{{ route('admin.crm.index') }}" method="get">
+            <div class="row">
+                <div class="form-group col-md-3 col-lg-2">
+                    <select name="is_active" id="is-active-filter" class="form-control" ng-model="isActiveFilter">
+                        <option value="">Customer Status</option>
+                        <option value="active">Active</option>
+                        <option value="inactive">Inactive</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-3 col-lg-2">
+                    <select name="payment_status" id="payment-status-filter" class="form-control" ng-model="paymentStatusFilter">
+                        <option value="">Payment Status</option>
+                        <option value="paid">Paid</option>
+                        <option value="due">Due</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2 col-lg-1">
+                    <button class="btn btn-success btn-block" type="submit">
+                        <i class="fa fa-filter"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
         <table id="customers-table" class="table table-striped">
             <thead>
                 <tr>
+                    <th>SL</th>
                     <th>Name</th>
                     <th>Email</th>
                     <th>CNIC</th>
@@ -14,8 +38,9 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach($customers as $customer)
+                @foreach($customers as $key => $customer)
                 <tr>
+                    <td>{{ $key + 1 }}</td>
                     <td>{{ $customer->name }}</td>
                     <td>{{ $customer->email }}</td>
                     <td>{{ $customer->cnic }}</td>
@@ -23,7 +48,7 @@
                     <td>{{ $customer->address }}</td>
                     <td>{{ $customer->status }}</td>
                     <td>
-                        <a class="btn btn-sm btn-primary" href="{{ route('admin.crm.edit', $customer->id) }}"><i class="fa fa-edit"></i></a>
+                        {{-- <a class="btn btn-sm btn-primary" href="{{ route('admin.crm.edit', $customer->id) }}"><i class="fa fa-edit"></i></a> --}}
                         <a class="btn btn-sm btn-secondary" href="{{ route('admin.crm.show', $customer->id) }}"><i class="fa fa-eye"></i></a>
                     </td>
                 </tr>
@@ -32,10 +57,19 @@
         </table>
     </div>
 </div>
-
+@endsection
+@section('scripts')
+<script src="{{ asset('js/angular.min.js') }}"></script>
 <script>
+    const app = angular.module('crm', []);
+    app.controller('CRMCtrl', function($scope) {
+        $scope.isActiveFilter = '{{ $is_active }}';
+        $scope.paymentStatusFilter = '{{ $payment_status }}';
+    });
+
     $(function() {
         $("#customers-table").DataTable();
     });
 </script>
+    
 @endsection
