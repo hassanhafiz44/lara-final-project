06878ec8836c5685392c2477a83a5dc52deb6f91
Hassan <hassanhafiz44@gmail.com>
2020-11-24 17:00:46 +0500

    Services page updated

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -6,55 +6,41 @@
 				<div class="col-lg-4 col-md-6 mb-4">
 					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-wrench" aria-hidden="true"></i></span>
 					<h2 class="font-weight-light">Repairing</h2>
-				<p>We provide all type of repairing including displays, desktop PCs, Laptops within the city corners initially. </p>
 				</div>
 
 				<div class="col-lg-4 col-md-6 mb-4">
 					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-eye" aria-hidden="true"></i></span>
-					<h2 class="font-weight-light">Security cameras</h2>
-				<p>We provide all type of repairing including displays, desktop PCs, Laptops within the city corners initially.</p>
+					<h2 class="font-weight-light">Security Cameras</h2>
 				</div>
 
 				<div class="col-lg-4 col-md-6 mb-4" >
 					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-bug" aria-hidden="true"></i></span>
 					<h2 class="font-weight-light">Virus Cleaning</h2>
-				<p>We provide all type of repairing including displays, desktop PCs, Laptops within the city corners initially.</p>
 				</div>
 
 				<div class="col-lg-4 col-md-6 mb-4">
-					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-lock" aria-hidden="true"></i></span>
-					<h2 class="font-weight-light">customization</h2>
-				<p>We provide all type of repairing including displays, desktop PCs, Laptops within the city corners initially.</p>
+					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-desktop" aria-hidden="true"></i></span>
+					<h2 class="font-weight-light">Customization</h2>
 				</div>
 
 				<div class="col-lg-4 col-md-6 mb-4">
 					<span style="font-size: 50px; color: rgb(248,70,129)";><i class="fa fa-gamepad" aria-hidden="true"></i></span>
-					<h2 class="font-weight-light">Gaming rigs</h2>
-				<p>We provide all type of repairing including displays, desktop PCs, Laptops within the city corners initially.</p>
+					<h2 class="font-weight-light">Gaming Rigs</h2>
 				</div>
 
 				<div class="col-lg-4 col-md-6 mb-4">
-					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-lock" aria-hidden="true"></i></span>
-					<h2 class="font-weight-light">Custom builds</h2>
-				<p>Any text...</p>
-				</div>
-
-				<div class="col-lg-4 col-md-6 mb-4">
-					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-lock" aria-hidden="true"></i></span>
+					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-flag" aria-hidden="true"></i></span>
 					<h2 class="font-weight-light">Softwares</h2>
-				<p>Any text...</p>
 				</div>
 
 				<div class="col-lg-4 col-md-6 mb-4">
 					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-key" aria-hidden="true"></i></span>
 					<h2 class="font-weight-light">licensed Producs</h2>
-				<p>Any text...</p>
 				</div>
 
 				<div class="col-lg-4 col-md-6 mb-4">
 					<span style="font-size: 50px; color: rgb(248,70,129);"><i class="fa fa-lock" aria-hidden="true"></i></span>
 					<h2 class="font-weight-light">Security tools</h2>
-				<p>Any text...</p>
 				</div>
 			</div>
 		</div>
