fe17ae4e3b54a05ec6050535ecdab210e8fa146c
Hassan <hassanhafiz44@gmail.com>
2020-11-26 15:40:27 +0500

    fixed cards with flex on home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,31 +16,35 @@
 <div class="container">
   @if(Auth::guard('customers')->check())
   <div class="row text-white">
-    <div class="col-lg-3 mt-2 mb-2">
+    <div class="col-12 col-lg-3 mt-2 mb-2">
       <div class="card bg-primary">
-        <div class="card-body">
-          <h5 class="card-title">{{ __('labels.due_invoices') }} <span class="pull-right">{{ $no_due_invoices ?? 0 }}</span></h5>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <span class="card-text">{{ __('labels.due_invoices') }}</span>
+          <span class="card-text">{{ $no_due_invoices ?? 0 }}</span>
         </div>
       </div>
     </div>
-    <div class="col-lg-3 mt-2 mb-2">
+    <div class="col-12 col-lg-3 mt-2 mb-2">
       <div class="card bg-primary">
-        <div class="card-body">
-          <h5 class="card-title">{{ __('labels.due_amount') }} <span class="pull-right">${{ $due_total ?? 0}}</span></h5>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <span class="card-text">{{ __('labels.due_amount') }}</span>
+          <span class="card-text">{{ $due_total ?? 0 }}</span>
         </div>
       </div>
     </div>
-    <div class="col-lg-3 mt-2 mb-2">
+    <div class="col-12 col-lg-3 mt-2 mb-2">
       <div class="card bg-primary">
-        <div class="card-body">
-          <h5 class="card-title">{{ __('labels.paid_invoices') }}<span class="pull-right">{{ $no_paid_invoices ?? 0}}</span></h5>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <span class="card-text">{{ __('labels.paid_invoices') }}</span>
+          <span class="card-text">{{ $no_paid_invoices ?? 0 }}</span>
         </div>
       </div>
     </div>
-    <div class="col-lg-3 mt-2 mb-2">
+    <div class="col-12 col-lg-3 mt-2 mb-2">
       <div class="card bg-primary">
-        <div class="card-body">
-          <h5 class="card-title">{{ __('labels.paid_amount') }} <span class="pull-right">${{ $paid_total ?? 0}}</span></h5>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <span class="card-text">{{ __('labels.paid_amount') }}</span>
+          <span class="card-text">{{ $paid_total ?? 0 }}</span>
         </div>
       </div>
     </div>
@@ -48,15 +52,17 @@
   <div class="row text-white">
     <div class="col-lg-6 mt-2 mb-2">
       <div class="card bg-danger">
-        <div class="card-body">
-          <h5 class="card-title">{{ __('labels.cancelled_invoices') }} <span class="pull-right">{{ $no_canceled_invoices ?? 0}}</span></h5>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <span class="card-text">{{ __('labels.cancelled_invoices') }}</span>
+          <span class="card-text">{{ $no_canceled_invoices ?? 0 }}</span>
         </div>
       </div>
     </div>
     <div class="col-lg-6 mt-2 mb-2">
       <div class="card bg-danger">
-        <div class="card-body">
-          <h5 class="card-title">{{ __('labels.total_invoices') }} <span class="pull-right">{{ $no_total_invoices ?? 0}}</span></h5>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <span class="card-text">{{ __('labels.total_invoices') }}</span>
+          <span class="card-text">{{ $no_total_invoices ?? 0 }}</span>
         </div>
       </div>
     </div>
@@ -65,10 +71,8 @@
   <section class="text-gray-700 body-font">
 	<div class="container mx-auto flex px-5 py-12 flex-col md:flex-row items-center">
 		<div class="flex flex-col mb-16 items-center text-center md:w-1/2 md:pr-16 md:items-start md:text-left md:mb-0 lg:flex-grow">
-			<p class="title-font text-3xl mb-4 font-medium text-gray-900 sm:text-4xl">Before they sold out
-				<br class="hidden lg:inline-block">readymade by gluten
-      </p>
-			<p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray</p>
+			<p class="title-font text-3xl mb-4 font-medium text-gray-900 sm:text-4xl">Bilal Computers</p>
+			<p class="mb-8 leading-relaxed">Selling devices in a low price. Heavy in performance but light on pocket. Login and start shopping.</p>
 		</div>
 		<div class="w-5/6 md:w-1/2 lg:w-full lg:max-w-lg">
 			<img class="object-cover object-center rounded" src="{{ asset('storage/static/home/04.jpg') }}">
