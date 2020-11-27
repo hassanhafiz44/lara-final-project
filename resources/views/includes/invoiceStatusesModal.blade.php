dd587a721f5e7b39d7d27f1c3a1bc0b37c84aec6
Hassan <hassanhafiz44@gmail.com>
2020-11-27 21:10:52 +0500

    separated customer invoice status from it

--- resources/views/includes/invoiceStatusesModal.blade.php
+++ resources/views/includes/invoiceStatusesModal.blade.php
@@ -8,7 +8,6 @@
             <!-- Modal body -->
             <div class="modal-body">
                 <div class="container">
-                    @if (Auth::guard('web')->check())
                     <div class="form-group">
                         <label for="payment-status">@lang('labels.payment_status')</label>
                         <select ng-model="payment_status" class="form-control form-control-sm" id="payment-status" ng-change="onPaymentStatusChange()">
@@ -25,25 +24,6 @@
                             <option ng-if="payment_status !== 'paid'" value="canceled">@lang('labels.cancelled')</option>
                         </select>
                     </div>
-                    @endif
-                    @if (Auth::guard('customers')->check())
-                    <div class="form-group">
-                        <label for="payment-status">@lang('labels.payment_status')</label>
-                        <select disabled ng-model="payment_status" class="form-control form-control-sm" id="payment-status" ng-change="onPaymentStatusChange()">
-                            <option ng-if="invoice_status !== 'canceled'" value="paid">@lang('labels.paid')</option>
-                            <option ng-if="invoice_status !== 'delivered'" value="due">@lang('labels.due')</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="invoice-status">@lang('labels.invoice_status')</label>
-                        <select ng-model="invoice_status" class="form-control form-control-sm" id="invoice-status" ng-change="onInvoiceStatusChange()">
-                            <option value="ready">@lang('labels.ready')</option>
-                            <option value="processing">@lang('labels.processing')</option>
-                            <option ng-if="payment_status === 'paid'" value="delivered">@lang('labels.delivered')</option>
-                            <option ng-if="payment_status !== 'paid'" value="canceled">@lang('labels.cancelled')</option>
-                        </select>
-                    </div>
-                    @endif
                 </div>
             </div>
 
