151aac3fa86ae8ca0aa8b375f55e4c73a012c3c4
Hassan <hassanhafiz44@gmail.com>
2020-11-28 22:50:51 +0500

    login and sign up with AOS

--- resources/views/customers/auth/register.blade.php
+++ resources/views/customers/auth/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
+<div class="container" data-aos="fade-up">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
