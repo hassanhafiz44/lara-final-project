eab7695cc351f10642f1360d87d6cc865c41f08d
Hassan <hassanhafiz44@gmail.com>
2020-12-06 17:56:50 +0500

    Fixed contact us about us

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="my-5" data-aos="fade-left">
-    <div class="row">
-        <div class="card col-12 col-md-6 offset-md-6">
-            <div class="card-body text-justify">
-                <h1 class="card-title">{{ __('labels.about_us') }}</h1>
-                <p class="card-text">
-                    Welcome to Bilal Computers , your number one source for all product. We're dedicated to giving you the very best of computer product, with a focus on your desire and your need.<br>
-                    Bilal Computers has come a long way from its beginnings to this stage in order to serve you according to your need and wants. We now serve customers all over country and are thrilled that we're able to turn our passion into our own website.<br>
-                    We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments please don't hesitate to contact us.
-                </p>
-                <p>Sincerely,</p>
-                <p>[Mr. Bilal Dar]</p>
-            </div>
+<div class="row">
+    <div class="card col-12 col-md-6 offset-md-6">
+        <div class="card-body text-justify">
+            <h1 class="card-title">{{ __('labels.about_us') }}</h1>
+            <p class="card-text">
+                Welcome to Bilal Computers , your number one source for all product. We're dedicated to giving you the very best of computer product, with a focus on your desire and your need.<br>
+                Bilal Computers has come a long way from its beginnings to this stage in order to serve you according to your need and wants. We now serve customers all over country and are thrilled that we're able to turn our passion into our own website.<br>
+                We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments please don't hesitate to contact us.
+            </p>
+            <p>Sincerely,</p>
+            <p>[Mr. Bilal Dar]</p>
         </div>
     </div>
 </div>
--- resources/views/pages/contactus.blade.php
+++ resources/views/pages/contactus.blade.php
@@ -1,66 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="head pb-5" data-aos="fade-right">
-    <div class="container py-5">
-        <div class="card">
-            <div class="card-body">
-                <h1 class="font-weight-bold text-center py-3">{{ __('labels.contact_us') }}</h1>
-                <div class="row">
-                    <div class="col-lg-6 col-md-12 col-sm-12 col-12">
-                        <div class="row pt-3">
-                            <div class="col-lg-1 offset-1 col-md-2 col-sm-2 col-2">
-                                <span style="font-size: 35px; color: red;"><i class="fa fa-map-marker" aria-hidden="true"></i>
-                                </span>
-                            </div>
-                            <div class="col-lg-10 col-md-9 col-sm-9 col-9">
-                                <h3 class="font-weight-light">Find us here</h3>
-                                <p>Neika Pura, Pul Aik,<br> Sialkot, Pakistan </p>
-                            </div>
-                        </div>
-                        <div class="row pt-3">
-                            <div class="col-lg-1 offset-1 col-md-2 col-sm-2 col-2">
-                                <span style="font-size: 35px; color: green;"><i class="fa fa-phone" aria-hidden="true"></i>
-                                </span>
-                            </div>
-                            <div class="col-lg-10 col-md-9 col-sm-9 col-9">
-                                <h3 class="font-weight-light">Give us a ring</h3>
-                                <p>Bilal Dar<br>
-                                    0300-1234567<br> 9AM - 8PM
-                                </p>
-                            </div>
-                        </div>
+<div class="row">
+    <div class="card col-md-6 offset-md-6">
+        <div class="card-body">
+            <h1 class="card-title">{{ __('labels.contact_us') }}</h1>
+            <div class="d-flex">
+                <span class="text-center h1 text-danger align-items-center mr-5"><i class="fa fa-map-marker"></i></span>
+                <div>
+                    <p class="h2">Find Us Here</p>
+                    <span class="card-text">Neika Pura, Pul Aik,</span>
+                    <span class="card-text">Sialkot, Pakistan</span>
+                </div>
+            </div>
 
-                    </div>
-
-                    <div class="col-lg-6 col-md-12 col-sm-12 col-12">
-                        <form action="{{ route('pages.submit.contact.us') }}" method="POST">
-                            @csrf
-                            <div class="form-row">
-                                <div class="form-group col-sm-12 col-md-12 col-12">
-                                    <label for="name">Name</label>
-                                    <input type="text" id="name" class="form-control" value="{{ $customer->name }}" placeholder="Enter your name" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" id="email" class="form-control" placeholder="Enter your email" readonly value="{{ $customer->email}}">
-                            </div>
-                            <div class="form-group">
-                                <label for="message">Message</label>
-                                <textarea name="message" id="message" class="form-control mb-3" placeholder="Write your message here" minlength="15" maxlength="500" required></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Send Message</button>
-
-                        </form>
-
-                    </div>
+            <div class="d-flex mt-5">
+                <span class="text-center h1 text-success align-items-center mr-5"><i class="fa fa-phone"></i></span>
+                <div>
+                    <p class="h2">Give Us a Ring</p>
+                    <span class="card-text">Bilal Dar</span>
+                    <span class="card-text">0300-1234567</span>
                 </div>
             </div>
         </div>
     </div>
-
-</section>
+</div>
 @endsection
 
 @section('scripts')
